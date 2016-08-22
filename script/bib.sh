37c73a5291025a85a45b3eb69fb4ead5141ba19f
Daniel Baur <daniel.baur@uni-ulm.de>
2016-08-22 19:14:20 +0200

    Finished research page: added publications and project logos

--- script/bib.sh
+++ script/bib.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e # halt script on error
 
-bibtex2html -single -nodoc -o ../_includes/generated/publication ../_files/bibtex.bib
+bibtex2html -single -d -r -nodoc -o ../_includes/generated/publication ../_files/bibtex.bib
